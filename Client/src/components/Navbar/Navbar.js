d7661a0aa4096525c74165d9093eec08325b26bb
David Lucas <davidivanelucas@gmail.com>
2021-02-09 09:50:27 +0800

    add movie remove

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -65,7 +65,7 @@ function Navbar(props) {
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
         <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
+          {/* <li className="nav-item">
             <Link
               to="/add-movie"
               className="nav-links"
@@ -73,7 +73,7 @@ function Navbar(props) {
             >
               Add Movie
             </Link>
-          </li>
+          </li> */}
           <li className="nav-item">
             <Link to="/compare" className="nav-links" onClick={closeMobileMenu}>
               Compare
